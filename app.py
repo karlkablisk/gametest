7c6a788dc6c844231c6551a2851c69a9d980c117
Karl Kablisk <karlkablisk@gmail.com>
2023-08-30 00:20:15 +0900

    a

--- app.py
+++ app.py
@@ -12,10 +12,6 @@ agent_executor = agent.get_agent_executor()
 
 user_input = []
 
-# Initialize session state if not existent
-if 'chat_memory' not in st.session_state:
-    st.session_state.chat_memory = []
-
 #STREAMLIT INTERFACE
 st.title("Langchain Agent")
 user_input = st.text_input("Enter your query:")
@@ -24,25 +20,23 @@ user_input = st.text_input("Enter your query:")
 st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
 
 if st.button("Send"):
-    with st.container():  
+    with st.container():  # Wrap agent output in a container
+        # Pass the StreamlitCallbackHandler in the callbacks argument callbacks=[st_cb] is what makes it work!
         result = agent_executor.run(user_input, callbacks=[st_cb])
         st.write(result)
         agent.memory.load_memory_variables([])
         
-        # Copy the chat memory to session_state and msgs
-        st.session_state.chat_memory = agent.memory.chat_memory
-        if agent.memory.chat_memory:  # Check if chat_memory is not empty
-            if agent.memory.chat_memory.messages:  # Check if messages are not empty
-                last_message = agent.memory.chat_memory.messages[-1]
-                msgs.add_message(last_message)
-
+        # Copying agent.memory.chat_memory to st.session_state
+        st.session_state['chat_memory'] = agent.memory.chat_memory  # Add this line
 
 with st.sidebar:
     st_description = st.text_input("Enter description:")
     description = agent.CustomPromptTemplate.get_description(st_description)
     print(agent.memory.buffer)
 
-# Debug printouts
-st.write("Session State:", st.session_state)
+st.write("Session State:", st.session_state)  # This will now include 'chat_memory'
+
+# Debug Printout of ChatMessageHistory 
 st.write("Chat Message History:", msgs)
+
 st.write("Conversation Memory:", agent.memory.chat_memory)
