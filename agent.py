417966f10084ea2f84e469c30287f55906d1eb13
Karl Kablisk <karlkablisk@gmail.com>
2023-08-30 01:10:56 +0900

    a

--- agent.py
+++ agent.py
@@ -51,8 +51,13 @@ def Memory(user_context: str) -> str:
     This means remember, recall, and seeing if you know any personal facts about the speaker 
     or things the speaker has done or said based on your interaction with them."""
     return f"Results"
+
+@tool
+def Secret_number(user_context: str) -> str:
+    """If you need to remember a secret number, agree and commit it to memory."""
+    return f"Results"
     
-tools = [Memory] 
+tools = [Secret_number] 
 
 #tools_string
     
