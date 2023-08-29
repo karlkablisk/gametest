77fd07417f858970c799a78f10188b14bed8b38f
Karl Kablisk <karlkablisk@gmail.com>
2023-08-29 17:38:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-#app.py
 import streamlit as st
 from langchain.callbacks import StreamlitCallbackHandler
 import agent
@@ -9,28 +8,24 @@ load_dotenv()
 # Initialize the agent executor
 agent_executor = agent.get_agent_executor()
 
-#STREAMLIT INTERFACE
-st.title("Langchain Agent")
-
-# Display a message
-st.write("This tool let's you talk to the ai")
-
-# Get user input
-user_input = st.text_input("Enter your query:", key="user_input_key")
-
 # Initialize StreamlitCallbackHandler
 st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
 
+#STREAMLIT INTERFACE
+st.title("Langchain Agent")
+st.write("This tool let's you talk to the ai")
+
+input_placeholder = st.empty()
+user_input = input_placeholder.text_input("Enter your query:", key="user_input_key")
+
 if st.button("Send"):
     with st.container():  # Wrap agent output in a container
-        # Pass the StreamlitCallbackHandler in the callbacks argument callbacks=[st_cb] is what makes it work!
         result = agent_executor.run(user_input, callbacks=[st_cb])
         st.write(result)
         agent.memory.load_memory_variables([])
 
-    # Clear the text input and focus back on it
-    st.session_state.user_input_key = ""  # Clearing the textbox
-    st.write('<input type="text" name="user_input_key" id="user_input_key" autofocus />', unsafe_allow_html=True)
+    # Clear the text input
+    input_placeholder.text_input("Enter your query:", value='', key="user_input_key")
 
 with st.sidebar:
     st_description = st.text_input("Enter description:")
