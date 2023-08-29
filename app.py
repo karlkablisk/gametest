e74fc7ffad751484403358492efb58a68b0dc474
Karl Kablisk <karlkablisk@gmail.com>
2023-08-29 16:28:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -22,10 +22,16 @@ user_input = st.text_input("Enter your query:")
 st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
 
 if st.button("Send"):
+    global conversation
     with st.container():  # Wrap agent output in a container
+        if conversation is None:  # If conversation is not initialized, initialize it
+            conversation = agent.get_conversation_chain()
+        # Add the user input into the conversation
+        conversation.add_to_memory("Human", user_input)
         # Pass the StreamlitCallbackHandler in the callbacks argument callbacks=[st_cb] is what makes it work!
-        result = agent_executor.run(user_input, callbacks=[st_cb])
+        result = agent_executor.run(user_input, callbacks=[st_cb], conversation=conversation)
         st.write(result)
+
 
 with st.sidebar:
     st_description = st.text_input("Enter description:")
