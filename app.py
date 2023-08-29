ce523f3a7bf7f97bbbac2fe80c41b84e62aeea0c
Karl Kablisk <karlkablisk@gmail.com>
2023-08-30 00:13:04 +0900

    a

--- app.py
+++ app.py
@@ -7,13 +7,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 # Initialize the agent executor
 agent_executor = agent.get_agent_executor()
 
 user_input = []
 
-
+# Initialize session state if not existent
+if 'chat_memory' not in st.session_state:
+    st.session_state.chat_memory = []
 
 #STREAMLIT INTERFACE
 st.title("Langchain Agent")
@@ -23,21 +24,22 @@ user_input = st.text_input("Enter your query:")
 st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
 
 if st.button("Send"):
-    with st.container():  # Wrap agent output in a container
-        # Pass the StreamlitCallbackHandler in the callbacks argument callbacks=[st_cb] is what makes it work!
+    with st.container():  
         result = agent_executor.run(user_input, callbacks=[st_cb])
         st.write(result)
         agent.memory.load_memory_variables([])
-
+        
+        # Copy the chat memory to session_state and msgs
+        st.session_state.chat_memory = agent.memory.chat_memory
+        if agent.memory.chat_memory:  # Check if chat_memory is not empty
+            msgs.append(agent.memory.chat_memory[-1])
 
 with st.sidebar:
     st_description = st.text_input("Enter description:")
     description = agent.CustomPromptTemplate.get_description(st_description)
     print(agent.memory.buffer)
 
+# Debug printouts
 st.write("Session State:", st.session_state)
-
-# Debug Printout of ChatMessageHistory 
 st.write("Chat Message History:", msgs)
-
 st.write("Conversation Memory:", agent.memory.chat_memory)
