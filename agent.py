024e421f0d197912d1ed4d8ea26b02e435dde9b0
Karl Kablisk <karlkablisk@gmail.com>
2023-08-29 16:35:37 +0900

    Update agent.py

--- agent.py
+++ agent.py
@@ -67,7 +67,7 @@ msgs = ChatMessageHistory(key="memory_key")
 embeddings = OpenAIEmbeddings()
 
 #memory start
-memory = ConversationBufferMemory(memory_key="history", chat_memory=msgs, input_key="input", return_messages=True)
+memory = ConversationBufferMemory(memory_key="history", input_key="input", return_messages=True)
 
 
 docs = [Document(page_content=t.description, metadata={"index": i}) for i, t in enumerate(ALL_TOOLS)]
