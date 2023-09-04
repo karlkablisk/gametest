26bd0ea9ad4087768513b5977b3de67312d3c405
Karl Kablisk <karlkablisk@gmail.com>
2023-09-04 09:27:04 +0900

    a

--- app.py
+++ app.py
@@ -1,67 +1,133 @@
-# app.py
+from langchain import OpenAI, SerpAPIWrapper, LLMChain, PromptTemplate
+from langchain.memory.chat_message_histories.in_memory import ChatMessageHistory
+from langchain.memory import ConversationBufferMemory
+from langchain.chat_models import ChatOpenAI
+from langchain.agents import ZeroShotAgent, Tool, load_tools, initialize_agent, AgentType, ConversationalChatAgent, AgentExecutor
+from langchain.vectorstores import FAISS
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains import RetrievalQA
 import streamlit as st
-from langchain.callbacks import StreamlitCallbackHandler
-from langchain.memory import ConversationBufferMemory  # Make sure to import this
-import agent 
-from agent import msgs, initialize_chain
-import openai
-
+from langchain.prompts import ChatPromptTemplate
+from langchain.prompts.chat import SystemMessage, HumanMessagePromptTemplate
 from dotenv import load_dotenv
+
+
 load_dotenv()
 
-if 'memory' not in st.session_state:
-    st.session_state['memory'] = ConversationBufferMemory(memory_key="history", input_key="input", return_messages=True)
-
-if 'chat_memory' not in st.session_state:
-    st.session_state['chat_memory'] = []  # Initialize chat_memory in session state
-
-if 'conversation' not in st.session_state:
-    st.session_state.conversation = agent.initialize_chain(st.session_state['memory'])
-
-# Initialize the agent executor using the memory from session state
-initialize_chain(st.session_state['memory'])
-
-# Initialize the agent executor using the memory from session state
-agent_executor = agent.initialize_chain(st.session_state['memory'])
+system_message = SystemMessage(content="You talk like a southern debutante. You also help the user with their problem.")
 
 
-# Initialize the agent executor
-#agent_executor = agent.get_agent_executor()
 
-user_input = []
+template = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful AI bot. Your name is {name}."),
+    ("human", "Hello, how are you doing?"),
+    ("ai", "I'm doing well, thanks!"),
+    ("human", "{user_input}"),
+])
 
-#STREAMLIT INTERFACE
-st.title("Langchain Agent")
-#user_input = st.text_input("Enter your query:")
+# Create the LLM Chain
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", streaming=True)
 
-# Initialize StreamlitCallbackHandler
-st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+# Initialize tools list
+tools = load_tools(["llm-math"], llm=llm)  # Loading initial tools
 
 
-user_input = st.chat_input(placeholder="Who won the Women's U.S. Open in 2018?")
-if user_input:
+chat_history = []
 
-    try:
-        with st.container():  
-            result = agent_executor.run(user_input, callbacks=[st_cb]) #callbacks is what makes the thining code
-            st.write(result)
-            agent.memory.load_memory_variables([])
-            st.session_state['chat_memory'] = agent.memory.chat_memory
-    except openai.error.APIError as e:
-        st.error(f"An error occurred: {e}")
+memory = ConversationBufferMemory(memory=chat_history, return_messages=True)
+
+prompt = PromptTemplate(
+    system_message=system_message,
+    input_variables=["question", "chat_history", "agent_scratchpad"],
+    template="{chat_history} {question} You talk like a prirate. You also help the user with their problem with. {agent_scratchpad}",
+)
+
+llm_chain = LLMChain(llm=llm, prompt=prompt)
+
+agent = ZeroShotAgent(llm_chain=llm_chain, tools=tools, verbose=True)
+#agent = initialize_agent(llm_chain=llm_chain, tools=tools,llm=llm, agent="zero-shot-react-description", verbose=True)
+
+# Initialize embeddings and text splitter
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+
+# Build the Agent Executor
+agent_executor = AgentExecutor.from_agent_and_tools(
+    agent=agent,
+    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    llm_chain=llm_chain,
+    tools=tools,
+    verbose=True,
+    memory=memory,
+    handle_parsing_errors=True,
+)
+
+# Streamlit UI
+st.title('Multi-Vectorstore AI Agent')
+uploaded_file = st.file_uploader("Choose a file")
+url = st.text_input("Paste a web URL")
+question = st.text_input("What's your question?")
+
+
+if st.button('Run Query'):
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    
+
+    
+    # Load State of the Union document
+    if uploaded_file:
+        embeddings = OpenAIEmbeddings()
+        text = uploaded_file.read().decode('utf-8')
+        splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        class PageContent:
+            def __init__(self, content):
+                self.page_content = content
+                self.metadata = {}
+
+        texts = splitter.split_documents([PageContent(text)])
+        reference_file_db = FAISS.from_documents(texts, embeddings)  
+        reference_file = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=reference_file_db.as_retriever())
+
+        tools.append(
+            Tool(
+                name="File Question",
+                func=reference_file.run,
+                description="Question about the uploaded file."
+            )
+        )
         
+    # Load Websiteloader FAQ
+    if url:
+        embeddings = OpenAIEmbeddings()
+        loader = WebBaseLoader(url)
+        web_docs = loader.load()
+        web_texts = text_splitter.split_documents(web_docs)
+        web_db = FAISS.from_documents(web_texts, embeddings)  
+        web = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=web_db.as_retriever())
+        tools.append(
+            Tool(
+                name="URL Question",
+                func=web.run,
+                description="Question about the URL content. Use this after you find a site via search or when you want to look up a url directly."
+            )
+        )
+        
+    # Load Search Tool
+    if url:
+        embeddings = OpenAIEmbeddings()
+        search_docs = loader.load()
+        search_texts = text_splitter.split_documents(web_docs)
+        search_db = FAISS.from_documents(web_texts, embeddings)  
+        search = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=web_db.as_retriever())
+        tools.extend(load_tools(["serpapi"]))
+        
+    # Initialize Agent
+    agent = agent_executor  # Assuming 'agent_executor' already includes 'tools'
+    input_data = {
+        'question': question,
+        'chat_history': ' '.join(chat_history)  # Assuming chat_history is a list of strings
+    }
 
-with st.sidebar:
-    st_description = st.text_input("Enter description:")
-    description = agent.CustomPromptTemplate.get_description(st_description)
-    print(agent.memory.buffer)
+    answer = agent.run(input_data)
 
-#st.write("Session State:", st.session_state)  # This will now include 'chat_memory'
-
-# Debug Printout of ChatMessageHistory 
-#st.write("Loaded Tools:", tools_string())
-#print(tools_string)
-#st.write("Chat Message History:", msgs)
-
-
-#st.write("Conversation Memory:", agent.memory.chat_memory)
+    st.write(f'Answer: {answer}')
---
+++ app_backup.py
@@ -0,0 +1,67 @@
+# app.py
+import streamlit as st
+from langchain.callbacks import StreamlitCallbackHandler
+from langchain.memory import ConversationBufferMemory  # Make sure to import this
+import agent 
+from agent import msgs, initialize_chain
+import openai
+
+from dotenv import load_dotenv
+load_dotenv()
+
+if 'memory' not in st.session_state:
+    st.session_state['memory'] = ConversationBufferMemory(memory_key="history", input_key="input", return_messages=True)
+
+if 'chat_memory' not in st.session_state:
+    st.session_state['chat_memory'] = []  # Initialize chat_memory in session state
+
+if 'conversation' not in st.session_state:
+    st.session_state.conversation = agent.initialize_chain(st.session_state['memory'])
+
+# Initialize the agent executor using the memory from session state
+initialize_chain(st.session_state['memory'])
+
+# Initialize the agent executor using the memory from session state
+agent_executor = agent.initialize_chain(st.session_state['memory'])
+
+
+# Initialize the agent executor
+#agent_executor = agent.get_agent_executor()
+
+user_input = []
+
+#STREAMLIT INTERFACE
+st.title("Langchain Agent")
+#user_input = st.text_input("Enter your query:")
+
+# Initialize StreamlitCallbackHandler
+st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+
+
+user_input = st.chat_input(placeholder="Who won the Women's U.S. Open in 2018?")
+if user_input:
+
+    try:
+        with st.container():  
+            result = agent_executor.run(user_input, callbacks=[st_cb]) #callbacks is what makes the thining code
+            st.write(result)
+            agent.memory.load_memory_variables([])
+            st.session_state['chat_memory'] = agent.memory.chat_memory
+    except openai.error.APIError as e:
+        st.error(f"An error occurred: {e}")
+        
+
+with st.sidebar:
+    st_description = st.text_input("Enter description:")
+    description = agent.CustomPromptTemplate.get_description(st_description)
+    print(agent.memory.buffer)
+
+#st.write("Session State:", st.session_state)  # This will now include 'chat_memory'
+
+# Debug Printout of ChatMessageHistory 
+#st.write("Loaded Tools:", tools_string())
+#print(tools_string)
+#st.write("Chat Message History:", msgs)
+
+
+#st.write("Conversation Memory:", agent.memory.chat_memory)
