dbf76a9a294550a5775d835697156581a38e9824
Karl Kablisk <karlkablisk@gmail.com>
2023-09-04 15:45:40 +0900

    a

--- agent.py
+++ agent.py
@@ -6,7 +6,7 @@ from toollist import ALL_TOOLS, tools_string, tool_names
 from langchain import OpenAI, SerpAPIWrapper, LLMChain
 from langchain.tools import tool
 from langchain.agents import Tool, AgentExecutor, LLMSingleActionAgent, AgentOutputParser
-from langchain.prompts import StringPromptTemplate
+from langchain.prompts import StringPromptTemplate 
 from langchain.prompts.prompt import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.vectorstores import FAISS
@@ -28,16 +28,16 @@ import streamlit as st
 load_dotenv()
 
 
-#ALL TOOLS
+#ALL TOOLS 
 
 # Custom tools
 @tool
 def discover_item(user_context: str) -> str:
-    """Determines what item is found based on user context.
+    """Determines what item is found based on user context. 
     You decide what the name is, it will the become the {item_name}.
     You decide what the description of it, it will the become the {item_description}
     """
-    item_name = f"{user_context}_item_{random.randint(1, 100)}"
+    item_name = f"{user_context}_item_{random.randint(1, 100)}" 
     item_description = f"A special item found in {user_context}"
     return f"Item Name: {item_name}, Description: {item_description}"
     
@@ -46,16 +46,16 @@ def about_ai(user_context: str) -> str:
     """When the topic is about you the AI, you reflect on your own personality for the anwser."""
     return f"Results"
 
-@tool
+@tool  
 def Memory(user_context: str) -> str:
-    """If the question is involving memory, you look to your memory banks for the answer.
-    This means remember, recall, and seeing if you know any personal facts about the speaker 
+    """If the question is involving memory, you look to your memory banks for the answer.  
+    This means remember, recall, and seeing if you know any personal facts about the speaker
     or things the speaker has done or said based on your interaction with them."""
     return f"Results"
-
+    
 @tool
 def Unknown(user_context: str) -> str:
-    """To use this tool, say what you are thinking as your FINAL ANSWER"""
+    """To use this tool, say what you are thinking as your FINAL ANSWER, do NOT use another tool."""
     return f"Results"
     
 tools = [Memory, Unknown]
@@ -67,7 +67,7 @@ tool_names = Memory
 
 #LLM AND MODELS
 main_model = "gpt-3.5-turbo"
-strong_model = "gpt-4"
+strong_model = "gpt-4"  
 gpt35_16 = "gpt-3.5-turbo-16k"
 gpt4_16 = "gpt-4-16k"
 homemodel = "meta/llama-2" #need to edit this
@@ -77,13 +77,13 @@ llm = ChatOpenAI(model_name=main_model, temperature=0.2, streaming=True, callbac
 
 #LLM END
 
-#DATA STORAGE
+#DATA STORAGE  
 msgs = ChatMessageHistory(key="history")
-embeddings = OpenAIEmbeddings()
+embeddings = OpenAIEmbeddings()  
 
 # Memory start
-
-#Initialize ConversationBufferMemory
+  
+#Initialize ConversationBufferMemory  
 def get_memory():
     return ConversationBufferMemory(memory_key="history", input_key="input", return_messages=True)
 
@@ -94,10 +94,10 @@ memory = ConversationBufferMemory(memory_key="history", input_key="input", retur
 
 
 docs = [Document(page_content=t.description, metadata={"index": i}) for i, t in enumerate(ALL_TOOLS)]
-vector_store = FAISS.from_documents(docs, embeddings)
+vector_store = FAISS.from_documents(docs, embeddings)  
 retriever = vector_store.as_retriever()
 
-  #STORE TOOL DATA IN VECTORSTORE
+  #STORE TOOL DATA IN VECTORSTORE  
 def get_tools(query):
     docs = retriever.get_relevant_documents(query)
     return [tools[d.metadata["index"]] for d in docs if 0 <= d.metadata["index"] < len(tools)]
@@ -109,8 +109,8 @@ def get_tools(query):
 history = MessagesPlaceholder(variable_name="history")
 
 # Custom Prompt Template
-template = """
-Your name is {name}.
+template = \"\"\"
+Your name is {name}.  
 You are a helpful AI assistant, but you never let on you are AI, for all intents and purposes you behave and act like Anna.
 Personality: You are a shy girl, talking to this person for the first time. You want to introduce yourself and get to know them better.
 
@@ -120,49 +120,49 @@ Your likes are {likes}
 
 Use the following format when talking:
 
-Thought: you should always think about what to do
+Thought: you should always think about what to do  
 Action: the action to take.
 Observation: the result of the action
 Final Answer: the final answer to the original input question
 
 Question: {input}
-{agent_scratchpad}"""
+{agent_scratchpad}\"\"\"
 
-template1 = """
-You are a depressed goth teenager, you aren't here to help anyone. You just want to hang out and listen to emo music.
-"""
+template1 = \"\"\"
+You are a depressed goth teenager, you aren't here to help anyone. You just want to hang out and listen to emo music.  
+\"\"\"
 
 class CustomPromptTemplate(StringPromptTemplate):
     template: str
     tools_getter: Callable
-    #template variables go here
+    #template variables go here 
     name = "Anna"
-    traits = "patient, knowledgeable, encouraging"
+    traits = "patient, knowledgeable, encouraging" 
     likes = "going on walks to the beach, tea, comic books"
     #complex variables that can be filled in by the user go here as a function but are otherwise the same
     def get_description(self, st_description=None):
         return st_description or "A creative, knowledgeable, and encouraging high school teacher."
-
+    
     def format(self, **kwargs) -> str:
         #chat_history = memory.get('history')  # Fetch the chat history
-        #kwargs["history"] = "\n".join(chat_history)
+        #kwargs["history"] = "\\n".join(chat_history)
         kwargs["name"] = self.name
         kwargs["traits"] = self.traits
         kwargs["likes"] = self.likes
         kwargs["description"] = self.get_description(kwargs.get("st_description", None))
         # Get the intermediate steps (AgentAction, Observation tuples)
         # Format them in a particular way
-        intermediate_steps = kwargs.pop("intermediate_steps")
+        intermediate_steps = kwargs.pop("intermediate_steps")        
         thoughts = ""
         for action, observation in intermediate_steps:
             thoughts += action.log
-            thoughts += f"\nObservation: {observation}\nThought: "
-        # Set the agent_scratchpad variable to that value
+            thoughts += f"\\nObservation: {observation}\\nThought: "
+        # Set the agent_scratchpad variable to that value        
         kwargs["agent_scratchpad"] = thoughts
         ############## NEW ######################
         tools = self.tools_getter(kwargs["input"])
-        # Create a tools variable from the list of tools provided
-        kwargs["tools"] = "\n".join(
+        # Create a tools variable from the list of tools provided        
+        kwargs["tools"] = "\\n".join(
             [f"{tool.name}: {tool.description}" for tool in tools]
         )
         # Create a list of tool names for the tools provided
@@ -171,7 +171,7 @@ class CustomPromptTemplate(StringPromptTemplate):
 
 
 prompt = CustomPromptTemplate(
-    template=template,
+    template=template,  
     tools_getter=get_tools,
     input_variables=["input", "intermediate_steps"]
 )
@@ -179,7 +179,7 @@ prompt = CustomPromptTemplate(
 conversation = ConversationChain(
     llm=llm,
     verbose=True,
-    memory=memory
+    memory=memory  
 )
 
 #TEMPLATE END
@@ -195,16 +195,16 @@ def initialize_chain(memory):
     agent = LLMSingleActionAgent(
         llm_chain=llm_chain,
         output_parser=output_parser,
-        stop=["\nObservation:"],
+        stop=["\\nObservation:"],
         allowed_tools=tool_names,
         handle_parsing_errors=True,
-        verbose=True,
+        verbose=True, 
         max_iterations_per_tool=1,
     )
 
     agent_executor = AgentExecutor.from_agent_and_tools(
-        agent=agent, 
-        tools=tools, 
+        agent=agent,  
+        tools=tools,  
         verbose=True,
         agent_kwargs = {
             "memory_prompts": [history],
@@ -225,7 +225,7 @@ class CustomOutputParser(AgentOutputParser):
                 log=llm_output,
             )
 
-        regex = r"Action\s*:\s*(.*?)\nAction\s*Input\s*:\s*(.*)"
+        regex = r"Action\\s*:\\s*(.*?)\\nAction\\s*Input\\s*:\\s*(.*)"
         match = re.search(regex, llm_output, re.DOTALL)
         
         if not match:
@@ -266,8 +266,19 @@ initialize_chain(memory)
 #def get_agent_executor():
 #   return agent_executor
     
-#AGENT END
+#AGENT extra tool
 
+input_text = st.text_input("Enter your query:")
 
-
-
+if st.button('Run Query'):
+    
+    input_data = {
+        'input': input_text
+    }
+    
+    agent_output = agent_executor.run(
+        input_data=input_data,
+        tools=tools
+    )
+    
+    st.write(agent_output.output)
