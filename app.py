d08200f8094788c0c595b9e58a27363b911bb018
Karl Kablisk <karlkablisk@gmail.com>
2023-09-10 15:07:20 +0900

    a

--- app.py
+++ app.py
@@ -1,83 +1,45 @@
+# app.py
 import streamlit as st
-from langchain import OpenAI, LLMChain
-from langchain.chat_models import ChatOpenAI
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.llms import OpenAI
-from langchain.chains import RetrievalQA
-from langchain.agents import initialize_agent, Tool, AgentType
-from langchain.document_loaders import TextLoader, WebBaseLoader
+from langchain.callbacks import StreamlitCallbackHandler
+import agent 
+from agent import msgs
 from dotenv import load_dotenv
-from langchain.utilities import SerpAPIWrapper
-from langchain.agents import load_tools
 
 load_dotenv()
 
-# Initialize embeddings and LLM.
-embeddings = OpenAIEmbeddings()
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", streaming=True)
+# Initialize the agent executor
+agent_executor = agent.get_agent_executor()
 
-# Streamlit UI
-st.title('AI Shadowrun')
+user_input = []
 
-uploaded_file = st.file_uploader("Choose a State of the Union file")
-url = st.text_input("Paste a Ruff FAQ URL")
-question = st.text_input("What's your question?")
+# Initialize session state if not existent
+if 'chat_memory' not in st.session_state:
+    st.session_state.chat_memory = []
 
+#STREAMLIT INTERFACE
+st.title("Langchain Agent")
+user_input = st.text_input("Enter your query:")
 
-if st.button('Run Query'):
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    
-    # Initialize tools list
-    tools = []
-    
-    # Load State of the Union document
-    if uploaded_file:
-        text = uploaded_file.read().decode('utf-8')
-        splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-        class PageContent:
-            def __init__(self, content):
-                self.page_content = content
-                self.metadata = {}
+# Initialize StreamlitCallbackHandler
+st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
 
-        texts = splitter.split_documents([PageContent(text)])
-        reference_file_db = FAISS.from_documents(texts, embeddings)  
-        reference_file = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=reference_file_db.as_retriever())
-
-        tools.append(
-            Tool(
-                name="File Question",
-                func=reference_file.run,
-                description="Question about the uploaded file."
-            )
-        )
+if st.button("Send"):
+    with st.container():  
+        result = agent_executor.run(user_input, callbacks=[st_cb])
+        st.write(result)
+        agent.memory.load_memory_variables([])
         
-    # Load Websiteloader FAQ
-    if url:
-        loader = WebBaseLoader(url)
-        ruff_docs = loader.load()
-        ruff_texts = text_splitter.split_documents(ruff_docs)
-        ruff_db = FAISS.from_documents(ruff_texts, embeddings)  
-        ruff = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=ruff_db.as_retriever())
-        tools.append(
-            Tool(
-                name="URL Question",
-                func=ruff.run,
-                description="Question about the URL content."
-            )
-        )
-        
-    # Load Search Tool
-    if url:
-        search_docs = loader.load()
-        search_texts = text_splitter.split_documents(ruff_docs)
-        search_db = FAISS.from_documents(ruff_texts, embeddings)  
-        search = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=ruff_db.as_retriever())
-        tools.extend(load_tools(["serpapi"]))
+        # Copy the chat memory to session_state and msgs
+        st.session_state.chat_memory = agent.memory.chat_memory
+        if agent.memory.chat_memory:  # Check if chat_memory is not empty
+            msgs.append(agent.memory.chat_memory[-1])
 
-        
-    # Initialize Agent
-    agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
-    answer = agent.run(question)
-    st.write(f'Answer: {answer}')
+with st.sidebar:
+    st_description = st.text_input("Enter description:")
+    description = agent.CustomPromptTemplate.get_description(st_description)
+    print(agent.memory.buffer)
+
+# Debug printouts
+st.write("Session State:", st.session_state)
+st.write("Chat Message History:", msgs)
+st.write("Conversation Memory:", agent.memory.chat_memory)
