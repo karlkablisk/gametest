6899af9b103c6d187f70c675a4e96320c1e0ce7f
Karl Kablisk <karlkablisk@gmail.com>
2023-09-11 17:37:39 +0900

    a

--- app.py
+++ app.py
@@ -1,84 +1,61 @@
 import streamlit as st
-from langchain.callbacks import StreamlitCallbackHandler
-import agent
-from agent import msgs
+from langchain import OpenAI, LLMChain
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.llms import OpenAI
+from langchain.chains import RetrievalQA
+from langchain.agents import initialize_agent, Tool, AgentType
+from langchain.document_loaders import TextLoader, WebBaseLoader
 from dotenv import load_dotenv
+from langchain.utilities import SerpAPIWrapper
+from langchain.agents import load_tools
 import requests
-import subprocess
+import json
 
 load_dotenv()
 
-# Webhook Configuration
-WEBHOOK_URL = st.secrets["WEBHOOK_URL"]
-
-# URL for the Flask app
-FLASK_URL = 'http://Karldiscordbottodb.karlkablisk.repl.co/messages'  # Replace with your Flask app URL
-
-# Initialize the agent executor
-agent_executor = agent.get_agent_executor()
-
-def fetch_messages():
-    response = requests.get(FLASK_URL)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return []
-
-def send_to_discord(message):
-    payload = {'content': message}
-    requests.post(WEBHOOK_URL, json=payload)
+# Initialize embeddings and LLM.
+embeddings = OpenAIEmbeddings()
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", streaming=True)
 
 # Streamlit UI
-st.title("Breeze-chan Chat")
-user_input = st.text_input("Enter your query:")
+st.title('Arrowtokyo.com AI Chat')
 
-# Initialize StreamlitCallbackHandler
-st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+# Fetching data directly from the URL and storing it as 'text'
+response = requests.get('https://arrowtokyo.com/en/arrow/rest/products')
+text = json.dumps(response.json())
 
-if st.button("Send"):
-    with st.container():
-        result = agent_executor.run(user_input, callbacks=[st_cb])
-        st.write(result)
-        agent.memory.load_memory_variables([])
-        send_to_discord(result)  # Sending to Discord
-        st.session_state['chat_memory'] = agent.memory.chat_memory
+question = st.text_input("What's your question?")
 
-# Sidebar
-with st.sidebar:
-    st_description = st.text_input("log:")
-    database_msgs = fetch_messages()
-    #st.write("Message History:", database_msgs)
+if st.button('Run Query'):
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    
+    # Initialize tools list
+    tools = []
+    
+    # Use the text fetched from the URL
+    if text:
+        splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        class PageContent:
+            def __init__(self, content):
+                self.page_content = content
+                self.metadata = {}
 
-    if st.button("Debug Test"):
-        payload = {'content': 'Debug message from Streamlit'}
-        response = requests.post(FLASK_URL, json=payload)
-        st.write(f"Debug Test Response: {response.status_code}")
-        try:
-            response_data = response.json()
-            received_message = response_data.get('message', 'No message received')
-            st.write("Received message:", received_message)
-        except ValueError:
-            st.write("Invalid JSON response from server")
+        texts = splitter.split_documents([PageContent(text)])
+        reference_file_db = FAISS.from_documents(texts, embeddings)  
+        reference_file = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=reference_file_db.as_retriever())
 
-# Debug printouts
-#st.write("Session State:", st.session_state)
-#st.write("Chat Message History:", msgs)
-#st.write("Conversation Memory:", agent.memory.chat_memory)
-
-# Output Messages
-database_msgs = fetch_messages()
-#st.write("Message History:", database_msgs)
-
-# Trigger Streamlit with Discord message
-def trigger_streamlit_with_discord_message(message):
-    with st.container():
-        result = agent_executor.run(message, callbacks=[st_cb])
-        st.write(result)
-        agent.memory.load_memory_variables([])
-        send_to_discord(result)
-        st.session_state['chat_memory'] = agent.memory.chat_memory
-
-# Test trigger from Discord
-#if st.button("Test Streamlit Trigger from Discord"):
-#    test_message = "Test message from Discord"
-   # trigger_streamlit_with_discord_message(test_message)
+        tools.append(
+            Tool(
+                name="File Question",
+                func=reference_file.run,
+                description="Question about the uploaded file."
+            )
+        )
+        
+    # Initialize Agent
+    agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+    answer = agent.run(question)
+    st.write(f'Answer: {answer}')
---
+++ app_agentver.py
@@ -0,0 +1,84 @@
+import streamlit as st
+from langchain.callbacks import StreamlitCallbackHandler
+import agent
+from agent import msgs
+from dotenv import load_dotenv
+import requests
+import subprocess
+
+load_dotenv()
+
+# Webhook Configuration
+WEBHOOK_URL = st.secrets["WEBHOOK_URL"]
+
+# URL for the Flask app
+FLASK_URL = 'http://Karldiscordbottodb.karlkablisk.repl.co/messages'  # Replace with your Flask app URL
+
+# Initialize the agent executor
+agent_executor = agent.get_agent_executor()
+
+def fetch_messages():
+    response = requests.get(FLASK_URL)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return []
+
+def send_to_discord(message):
+    payload = {'content': message}
+    requests.post(WEBHOOK_URL, json=payload)
+
+# Streamlit UI
+st.title("Breeze-chan Chat")
+user_input = st.text_input("Enter your query:")
+
+# Initialize StreamlitCallbackHandler
+st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+
+if st.button("Send"):
+    with st.container():
+        result = agent_executor.run(user_input, callbacks=[st_cb])
+        st.write(result)
+        agent.memory.load_memory_variables([])
+        send_to_discord(result)  # Sending to Discord
+        st.session_state['chat_memory'] = agent.memory.chat_memory
+
+# Sidebar
+with st.sidebar:
+    st_description = st.text_input("log:")
+    database_msgs = fetch_messages()
+    #st.write("Message History:", database_msgs)
+
+    if st.button("Debug Test"):
+        payload = {'content': 'Debug message from Streamlit'}
+        response = requests.post(FLASK_URL, json=payload)
+        st.write(f"Debug Test Response: {response.status_code}")
+        try:
+            response_data = response.json()
+            received_message = response_data.get('message', 'No message received')
+            st.write("Received message:", received_message)
+        except ValueError:
+            st.write("Invalid JSON response from server")
+
+# Debug printouts
+#st.write("Session State:", st.session_state)
+#st.write("Chat Message History:", msgs)
+#st.write("Conversation Memory:", agent.memory.chat_memory)
+
+# Output Messages
+database_msgs = fetch_messages()
+#st.write("Message History:", database_msgs)
+
+# Trigger Streamlit with Discord message
+def trigger_streamlit_with_discord_message(message):
+    with st.container():
+        result = agent_executor.run(message, callbacks=[st_cb])
+        st.write(result)
+        agent.memory.load_memory_variables([])
+        send_to_discord(result)
+        st.session_state['chat_memory'] = agent.memory.chat_memory
+
+# Test trigger from Discord
+#if st.button("Test Streamlit Trigger from Discord"):
+#    test_message = "Test message from Discord"
+   # trigger_streamlit_with_discord_message(test_message)
