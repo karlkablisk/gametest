b3244c029f4c00caaa040144402304e6f57c4ef3
Karl Kablisk <karlkablisk@gmail.com>
2023-09-15 17:36:48 +0900

    a

--- app.py
+++ app.py
@@ -1,55 +1,53 @@
 import streamlit as st
 from langchain.callbacks import StreamlitCallbackHandler
 import agent
-from agent import msgs
 from dotenv import load_dotenv
 import requests
-import threading
 from threading import Thread
 import time
 
 load_dotenv()
 
+# URLs configuration
 FLASK_URL = 'http://Karldiscordbottodb.karlkablisk.repl.co/messages'
+DISCORD_FETCH_URL = 'http://Karldiscordbottodb.karlkablisk.repl.co/get_discord_messages'
+WEBHOOK_URL = st.secrets["WEBHOOK_URL"]  # Load the Webhook URL from secrets
+
+# Initialize the agent executor
 agent_executor = agent.get_agent_executor()
 
+# Initialize Streamlit UI elements
 st.title("Breeze-chan Chat")
 st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
-
 user_input = st.text_input("Enter your query:")
 
+# Function to send the AI response to Discord via Webhook
+def send_to_discord(message):
+    payload = {'content': message}
+    requests.post(WEBHOOK_URL, json=payload)
+
+# If the "Send" button is clicked
 if st.button("Send"):
     with st.container():
         result = agent_executor.run(user_input, callbacks=[st_cb])
         st.write(result)
         agent.memory.load_memory_variables([])
-        response = requests.post(FLASK_URL, data=result)
-        if response.status_code != 200:
-            st.write(f"Failed to send message to Discord bot, status code: {response.status_code}")
+        send_to_discord(result)  # Sending the AI response to Discord
 
-
+# Sidebar configuration
 with st.sidebar:
     st_description = st.text_input("log:")
 
-def trigger_streamlit_with_discord_message(message):
-    with st.container():
-        result = agent_executor.run(message, callbacks=[st_cb])
-        st.write(result)
-        agent.memory.load_memory_variables([])
-        response = requests.post(FLASK_URL, json={'content': result})
-        if response.status_code != 200:
-            st.write(f"Failed to send message to Discord bot, status code: {response.status_code}")
-
+# Function to fetch Discord messages and display them in Streamlit
 def fetch_discord_messages():
     while True:
-        response = requests.get('http://Karldiscordbottodb.karlkablisk.repl.co/get_discord_messages')
+        response = requests.get(DISCORD_FETCH_URL)
         if response.status_code == 200:
             discord_messages = response.json()
             for msg in discord_messages:
-                # Update Streamlit UI with new messages
                 st.write(f"{msg['username']}: {msg['message']}")
         time.sleep(5)  # Poll every 5 seconds
 
-# Run the fetch_discord_messages function in a separate thread
+# Start a separate thread to run the fetch_discord_messages function
 thread = Thread(target=fetch_discord_messages)
 thread.start()
